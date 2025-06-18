3d099759dd1b555d6ecda1b0f51be160a5f07ccf
MinttDrazi <martinditz@gmail.com>
2025-06-18 13:22:23 +0200

    Feature: Test basic API Calls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import axios from "axios";
+
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get("http://localhost:3000").then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data);
+  });
+};
 
 function App() {
-  return <h1>Test</h1>;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <button onClick={apiCall}>Make API Call</button>
+    </div>
+  );
 }
 
 export default App;
--- index.ts
+++ index.ts
@@ -1,5 +1,34 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { cors } from "hono/cors";
 
-console.log(greet("World"));
+const app = new Hono();
+
+// CORS Middleware
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/", (c) => {
+  // c.html funkce vytvoří HTML odpověď
+  return c.html("<h1>Hello, World!</h1>");
+});
+
+app.get("say", (c) => {
+  // c.text funkce vytvoří text odpověď
+  return c.text("Hello, World");
+});
+
+app.get("/json", (c) => {
+  // c.json funkce vytvoří JSON odpověď
+  return c.json({ firstName: "Franta", lastName: "Sádlo" });
+});
+
+serve(app, (info) => {
+  console.log(`Server listening at http://localhost:${info.port}`);
+});
